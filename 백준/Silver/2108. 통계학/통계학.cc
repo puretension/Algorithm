237a3a323c2c9ba29124229f26d4c42f9cea3361
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2023-12-29 00:15:57 +0900

    [Silver III] Title: 통계학, Time: 128 ms, Memory: 5932 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.cc
+++ 백준/Silver/2108. 통계학/통계학.cc
@@ -1,88 +1,62 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int n;		
-	double element;
-	vector <double> v;
-	double ans1 = 0;
-	double ans2 = 0;
-	double ans3 = 0;
-	double ans4 = 0;
+int main(){ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	cin >> n;
-	for(int i = 0; i < n; i++) {
-		cin >> element;
-		v.push_back(element);
-	}
+    int n;
+    int max;
+    double ans1,ans2,ans3,ans4;
 
-	for (int i = 0; i < n; i++) {
-		ans1 += v[i];
-	}
-	ans1 = round(ans1 / n);
+    cin >> n;
+    vector<double> a(n);
+
+    for(int i = 0;i<n;i++){
+        cin >> a[i];
+        ans1 += a[i];
+    }
+
+    ans1 = round(ans1/n); //1
+    if(ans1 == -0) ans1 = 0;
+
+    sort(a.begin(), a.end());
+
+    int mid = (n-1)/2;
+    ans2 = a[mid]; //2
     
-	if (ans1 == -0) {
-		ans1 = 0;
-	}
+    int cntArr[8001] = {0,};
 
+     for(int i = 0;i<n;i++)
+       (a[i] >= 0) ? cntArr[int(a[i])]++ : cntArr[int(fabs(a[i]))+4000]++;
 
-	sort(v.begin(), v.end());
-	if (n == 1) {
-		ans2 = v[0];
-	}
-	else {
-		ans2 = v[(n - 1) / 2];
-	}
+    max = 0;
 
-	
-	int count[8001] = { 0, };
-	for (int i = 0; i < n; i++) {
-		if (v[i] >= 0) {
-			count[int(v[i])]++;
-		}
-		else {
-			count[int(fabs(v[i])) + 4000]++;
-		}
-	}
+    for(int i = 0;i<8001;i++){
+        if(max < cntArr[i]){
+            max = cntArr[i];
+        }
+    }
 
-	int max = count[0];
-	int index = 0;
-	for (int i = 0; i < 8001; i++) {
-		if (max < count[i]) {
-			index = i;
-			max = count[i];
-		}
-	}
+      vector<double> ans3Arr;
 
-	vector <double> mv;
-	for (int i = 0; i < 8001; i++) {
-		if (max == count[i]) {
-			if (i <= 4000) {
-				mv.push_back(i);
-			}
-			else {
-				mv.push_back((i - 4000) * (-1));
-			}
-		}
-	}
+     for(int i = 0;i<8001;i++){
+        if(max == cntArr[i]){
+            (i<=4000) ? ans3Arr.push_back(i): ans3Arr.push_back((i-4000) * (-1));
+        }
+     }
 
-	// 정렬 필요. 두번째로 작은 최빈값 출력(핵심 로직)
-	sort(mv.begin(), mv.end());
-	// 최빈값이 하나인 경우 
-	if (mv.size() == 1) {
-		ans3 = mv[0];
-	}
-	else {
-		ans3 = mv[1];
-	}
+     sort(ans3Arr.begin(),ans3Arr.end());
 
+     (ans3Arr.size() == 1) ? ans3 = ans3Arr[0] : ans3 = ans3Arr[1]; // 3
 
-	ans4 = v[n - 1] - v[0];
+     ans4 = a[n - 1] - a[0]; //4
 
-	cout << ans1 << "\n";
-	cout << ans2 << "\n";
-	cout << ans3 << "\n";
-	cout << ans4 << "\n";
-
-	return 0;
+    cout << ans1 << "\n";
+    cout << ans2 << "\n";
+    cout << ans3 << "\n";
+    cout << ans4 << "\n";
+    
+        return 0;
 }
