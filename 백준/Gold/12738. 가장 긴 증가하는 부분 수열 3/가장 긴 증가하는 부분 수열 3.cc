b503724b4cb9d53676f22f1d404599d77dafe8f9
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-01-15 01:48:09 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 188 ms, Memory: 22252 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -0,0 +1,29 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+    typedef long long ll;
+    ll n, res;
+
+    int main(){ 
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+
+        cin >> n;
+
+        vector<ll>a(n);
+        vector<ll>lis;
+
+        for(int i = 0;i<n;i++)
+            cin >> a[i];
+
+        for(int i = 0;i<n;i++){
+            //전기줄이랑 똑같다..
+            auto it = lower_bound(lis.begin(), lis.end(), a[i]);
+             if(it == lis.end()) lis.push_back(a[i]);
+             else *it = a[i];
+        }
+
+        cout << lis.size();
+
+            return 0;
+    }
